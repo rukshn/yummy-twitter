67b87edff9c3b81e1cd05ea4d7682f2a058abb42
Rukshan <arkruka@gmail.com>
2015-08-28 22:57:46 +0530

    fixed errors in example

--- example/post_status.php
+++ example/post_status.php
@@ -8,8 +8,6 @@
   if (isset($_SESSION['access_token'])) {
     $status = "Hacked Raspberry Pi turned into artificial pancreas http://www.businessinsider.com/hacked-raspberry-pi-artificial-pancreas-2015-8";
 
-    $user_id = $_SESSION['access_token']['user_id'];
-
     $parameters = array('status' => $status);
     $request = make_request('POST', $parameters , 'statuses/update');
     print_r($request);
